93b74b903a9235e88c510a0938695dd5723b128a
Alister <alister.messi@gmail.com>
2022-06-05 18:25:09 +0100

    Initial Commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const http = require('http');
+const { getProducts, getProduct, createProduct, updateProduct, deleteProduct } = require('./controllers/productController');
+
+const PORT = process.env.PORT || 5000;
+
+const server = http.createServer((req, res) => {
+    if(req.url === '/api/products' && req.method === 'GET'){
+        getProducts(req, res);
+    }
+    else if(req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'GET'){
+        const id = req.url.split('/')[3];  ///api/products/1
+        
+        getProduct(req, res, id);
+    }
+    else if(req.url === '/api/products' && req.method === 'POST'){
+        createProduct(req, res);
+    }
+    else if(req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'PUT'){
+        const id = req.url.split('/')[3];  ///api/products/1
+        
+        updateProduct(req, res, id);
+    }
+    else if(req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'DELETE'){
+        const id = req.url.split('/')[3];  ///api/products/1
+        
+        deleteProduct(req, res, id);
+    }
+    else{
+        res.writeHead(404, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({"message": "Route not found"}));
+    }
+});
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
